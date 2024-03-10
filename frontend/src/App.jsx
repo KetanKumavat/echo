6ece1ba88df2cab2c78eb1e6905947544987c4af
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-03-10 16:19:37 +0530

    making email section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SpotlightCompo from "./components/SpotlightCompo";
 import Ellipse from "../../frontend/public/Ellipse 9.svg";
 const App = () => {
   const [response, setResponse] = useState("");
+  // eslint-disable-next-line no-unused-vars
   const [senderEmail, setSenderEmail] = useState("");
   const [data, setData] = useState(null);
   const [editable, setEditable] = useState(false);
@@ -65,58 +66,70 @@ const App = () => {
       <img
         src={Ellipse}
         alt="ellipse"
-        className="absolute aspect-square -mt-[30vh] ml-[25vh] pointer-events-none"
+        className="absolute aspect-square -mt-[28vh] ml-[25vh] opacity-35 pointer-events-none"
       />
-      <div className="w-3/4 bg-transparent border-2 p-16 z-50 backdrop-blur-md mt-[20vh] rounded-2xl h-3/4 ml-[25vh]">
-        <div className="flex-col justify-center items-center">
-          <label htmlFor="">name</label>
-          <input
-            type="name"
-            placeholder="Enter Your Name"
-            onChange={(e) => setSenderEmail(e.target.value)}
-            className="flex justify-center items-center bg-white text-black p-4"
-          />
-        </div>
-        <div>
-          <label htmlFor="">email</label>
-          <input
-            type="email"
-            placeholder="Enter Your Email Address"
-            onChange={(e) => setSenderEmail(e.target.value)}
-            className="w-1/2 flex justify-center items-center text-white p-4"
-          />
-        </div>
-        <div>
-          <label htmlFor="">subject</label>
-          <textarea
-            className="w-full bg-gray-800 text-white"
-            placeholder="Enter the subject of the email"
-            onChange={(e) => setResponse(e.target.value)}
-          />
-        </div>
-        <div>
-          {data !== null && editable ? (
-            <div className="w-full h-screen">
-              <textarea
-                className="w-full bg-gray-800 h-full text-white p-4"
-                value={data}
-                onChange={(e) => setData(e.target.value)}
-                cols={50}
-                rows={10}
+
+      <div className="h-screen w-full flex justify-center">
+        <div className="w-3/4 bg-[rgba(255,255,255,0.05)] p-16 z-50 backdrop-blur-8xl mt-[20vh] rounded-2xl h-3/4">
+          <div className="flex flex-col justify-center">
+            <div className="flex justify-start gap-24 w-full">
+              <div className="flex-col justify-center items-center">
+                <label htmlFor="">Name</label>
+                <input
+                  type="name"
+                  placeholder="Enter Your Name"
+                  onChange={(e) => setSenderEmail(e.target.value)}
+                  className="flex justify-center items-center placeholder-opacity-95 bg-zinc-200 placeholder-black rounded-xl w-full text-black font-semibold p-4"
+                />
+              </div>
+              <div>
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  placeholder="Enter Your Email Address"
+                  onChange={(e) => setSenderEmail(e.target.value)}
+                  className="flex font-semibold text-left justify-start placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 w-[33vh] text-md"
+                />
+              </div>
+            </div>
+            <br />
+            <div>
+              <label htmlFor="">Subject</label>
+              <input
+                className="w-1/2 flex font-semibold justify-center items-center placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 text-md"
+                placeholder="Enter the subject of the email"
+                onChange={(e) => setResponse(e.target.value)}
               />
             </div>
-          ) : (
-            <h1>Generating Email</h1>
-          )}
-        </div>
-        <div>
-          <button
-            className="bg-blue-500 flex justify-center items-center text-white p-4"
-            onClick={callBackend}>
-            Generate Email
-          </button>
+            <br />
+            <div>
+              <div>
+                <br />
+                {data !== null && editable ? (
+                  <div className="border-slate-400 rounded-xl bg-zinc-600 z-0 bg-opacity-30">
+                    <textarea
+                      className="w-full bg-transparent opacity-1 z-50 text-white font-semibold h-96 rounded-xl p-4"
+                      value={data}
+                      onChange={(e) => setData(e.target.value)}
+                    />
+                  </div>
+                ) : (
+                  <textarea
+                    className="w-full bg-gray-800 h-full text-white p-4"
+                    placeholder="Generated Email will be shown here"
+                    onChange={(e) => setData(e.target.value)}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <button
+        className="px-3 py-3 rounded-xl border border-neutral-600 text-black text-md font-bold bg-white/85 hover:bg-transparent hover:text-white transition duration-200"
+        onClick={callBackend}>
+        Generate Email
+      </button>
     </div>
   );
 };
--- frontend/src/components/SpotlightCompo.tsx
+++ frontend/src/components/SpotlightCompo.tsx
@@ -10,10 +10,10 @@ export default function SpotlightPreview() {
         fill="white"
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-6xl md:text-8xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-6xl md:text-8xl font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Echo
         </h1>
-        <p className="mt-4 text-3xl font-normal text-neutral-300 max-w-lg text-center mx-auto">
+        <p className="mt-4 text-3xl font-semibold text-neutral-300 max-w-lg text-center mx-auto">
           Say goodbye to writer's block.
         </p>
       </div>
