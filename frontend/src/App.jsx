7a46be9675f03cf3e128ce9c88eec2c7496b63a9
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-03-15 22:57:37 +0530

    responsiveness of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -91,46 +91,47 @@ const App = () => {
         <img
           src={Ellipse}
           alt="ellipse"
-          className="absolute aspect-square -mt-[28vh] ml-[25vh] opacity-35 pointer-events-none"
+          className="absolute aspect-square stransform scale-125 opacity-70 mt-40 scale-y-150 md:-mt-[28vh] md:ml-[40vh] md:opacity-35 md:scale-150 pointer-events-none z-0 "
         />
 
         <div className="h-screen flex justify-center" ref={myRef}>
-          <div className="w-3/4 bg-[rgba(255,255,255,0.05)] p-16 z-50 backdrop-blur-8xl mt-[18vh] rounded-2xl h-3/4">
-            <div className="flex flex-col justify-center">
-              <div className="flex justify-start gap-20 w-full">
-                <div className="flex-col justify-center items-center">
+          <div className="w-3/4 bg-[rgba(255,255,255,0.05)] p-16 z-50 backdrop-blur-8xl mt-[18vh] rounded-2xl h-3/4 transform scale-95 md:scale-100 md:border-none border">
+            <div className="flex flex-col justify-center md:scale-105">
+              <div className="flex flex-col md:flex-row gap-2 w-full">
+                <div className="flex flex-col flex-grow">
                   <label htmlFor="">Name</label>
                   <input
                     type="name"
                     placeholder="Enter Your Name"
                     onChange={(e) => setSender(e.target.value)}
-                    className="flex justify-center items-center placeholder-opacity-95 bg-zinc-200 placeholder-black text-lg rounded-xl w-full text-black font-semibold p-4"
+                    className="placeholder-opacity-95 bg-zinc-200 placeholder-black text-lg rounded-xl text-black font-semibold p-4"
                   />
                 </div>
-                <div>
+                <div className="flex flex-col flex-grow">
                   <label htmlFor="">Sender&apos;s Email</label>
                   <input
                     type="email"
                     placeholder="Enter Your Email Address"
                     onChange={(e) => setSenderEmail(e.target.value)}
-                    className="flex font-semibold text-left justify-start placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4 w-[33vh] text-md"
+                    className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
                   />
                 </div>
-                <div>
+                <div className="flex flex-col flex-grow">
                   <label htmlFor="">Recipient&apos;s Email</label>
                   <input
                     type="email"
                     placeholder="Enter Recipient's Email Address"
                     onChange={(e) => setReceiverEmail(e.target.value)}
-                    className="flex font-semibold text-left justify-start placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4 w-[33vh] text-md"
+                    className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
                   />
                 </div>
               </div>
+
               <br />
               <div>
                 <label htmlFor="">Subject</label>
                 <input
-                  className="w-1/2 flex font-semibold justify-center text-lg items-center placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 text-md"
+                  className="w-full flex font-semibold justify-center text-lg items-center placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 text-md"
                   placeholder="Enter the subject of the email"
                   onChange={(e) => setSubject(e.target.value)}
                 />
@@ -143,8 +144,9 @@ const App = () => {
                     <div className="border-slate-400 rounded-xl bg-zinc-200 z-0 bg-opacity-10">
                       <label htmlFor="">Email Body</label>
                       <textarea
-                        className="w-full bg-transparent opacity-1 z-50 text-white font-semibold h-96 rounded-xl p-4"
+                        className="w-full bg-transparent opacity-1 z-50 text-white font-semibold rounded-xl p-4 overflow-y-scroll"
                         value={data}
+                        rows={4}
                         onChange={(e) => setData(e.target.value)}
                       />
                     </div>
@@ -152,8 +154,9 @@ const App = () => {
                     <div>
                       <label htmlFor="">Email Body</label>
                       <textarea
-                        className="w-full bg-zinc-200 h-full text-black place-content-start placeholder-black font-semibold text-lg rounded-xl p-4"
-                        placeholder="Write or Generate Email"
+                        className="w-full bg-zinc-200 h-full text-black placeholder-black font-semibold text-lg rounded-xl p-4 my-auto mx-auto  overflow-y-auto"
+                        placeholder="Write or Generate an Email"
+                        rows={4}
                         onChange={(e) => setData(e.target.value)}
                       />
                     </div>
