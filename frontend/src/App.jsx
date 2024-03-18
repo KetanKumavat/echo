900800a91632ccc9cd7f2908c50325d167d24ee3
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-03-19 01:28:21 +0530

    nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,6 @@ import { IconBrandGoogle } from "@tabler/icons-react";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 
-
 const App = () => {
   const myRef = useRef(null);
   const executeScroll = () =>
@@ -46,14 +45,14 @@ const App = () => {
     }
   };
 
-    const handleLogout = async () => {
-      try {
-        await signOut(auth);
-        setUser(null);
-      } catch (error) {
-        console.error("Error signing out:", error);
-      }
-    };
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
 
   async function callBackend() {
     setLoading(true);
@@ -290,6 +289,15 @@ const App = () => {
       />
       <div className="w-full h-screen bg-black/[0.96] scale-110 md:scale-100 items-center z-50">
         <SpotlightCompo className="z-0" />
+        <div
+          className="w-full flex z-50 text-center justify-center items-center cursor-pointer absolute bottom-10 md:-bottom-8"
+          onTouchStart={scroll}>
+          <div className="container" onClick={executeScroll}>
+            <div className="chevron" onClick={executeScroll}></div>
+            <div className="chevron" onClick={executeScroll}></div>
+            <div className="chevron" onClick={executeScroll}></div>
+          </div>
+        </div>
         <div className="w-full flex flex-col justify-center items-center z-50">
           {user ? (
             <button
@@ -310,15 +318,6 @@ const App = () => {
         </div>
         {user ? (
           <>
-            <div
-              className="w-full flex z-50 text-center justify-center items-center cursor-pointer absolute bottom-10 md:-bottom-8"
-              onTouchStart={scroll}>
-              <div className="container" onClick={executeScroll}>
-                <div className="chevron" onClick={executeScroll}></div>
-                <div className="chevron" onClick={executeScroll}></div>
-                <div className="chevron" onClick={executeScroll}></div>
-              </div>
-            </div>
             <img
               src={Ellipse}
               alt="ellipse"
@@ -329,97 +328,99 @@ const App = () => {
               ref={myRef}
               id="scroll-down">
               <div className="w-3/4 bg-[rgba(255,255,255,0.05)] p-5 md:p-20  z-50 backdrop-blur-8xl mt-40 md:mt-[18vh] rounded-2xl transform scale-110 md:scale-100 md:border-none border h-fit md:h-fit  max-h-fit justify-center">
-                      <div className="flex flex-col justify-center md:scale-105">
-                        <div className="flex flex-col lg:flex-row md:col-auto gap-2 w-full ">
-                          <div className="flex flex-col flex-grow">
-                            <label htmlFor="">Name</label>
-                            <input
-                              type="name"
-                              placeholder="Enter Your Name"
-                              onChange={(e) => setSender(e.target.value)}
-                              value={user ? user.displayName : ""}
-                              className="placeholder-opacity-95 bg-zinc-200 placeholder-black text-lg rounded-xl text-black font-semibold p-4"
-                            />
-                          </div>
-                          <div className="flex flex-col flex-grow">
-                            <label htmlFor="">Sender&apos;s Email</label>
-                            <input
-                              type="email"
-                              placeholder="Enter Your Email Address"
-                              onChange={(e) => setSenderEmail(e.target.value)}
-                              value={user ? user.email : ""}
-                              className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
-                            />
-                          </div>
-                          <div className="flex flex-col flex-grow">
-                            <label htmlFor="">Recipient&apos;s Email</label>
-                            <input
-                              type="email"
-                              placeholder="Enter Recipient's Email Address"
-                              onChange={(e) => setReceiverEmail(e.target.value)}
-                              className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
-                            />
-                          </div>
-                        </div>
-
-                        <br />
-                        <div>
-                          <label htmlFor="">Subject</label>
-                          <input
-                            className="w-full flex font-semibold justify-center text-lg items-center placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 text-md"
-                            placeholder="Enter the subject of the email"
-                            onChange={(e) => setSubject(e.target.value)}
-                          />
-                        </div>
-                        <br />
-                        <div>
-                          <div>
-                            <br />
-                            {loading && editable ? (
-                              <div>
-                                <label htmlFor="">Email Body</label>
-                                <textarea
-                                  className="w-full bg-zinc-200 h-full text-black placeholder-black font-semibold text-lg rounded-xl my-auto mx-auto p-4 overflow-y-scroll"
-                                  value={data}
-                                  contentEditable="false"
-                                  placeholder="Generating Email..."
-                                  rows={6}
-                                  onChange={(e) => setData(e.target.value)}
-                                />
-                              </div>
-                            ) : (
-                              <>
-                                <label htmlFor="">Email Body</label>
-                                <textarea
-                                  className="w-full bg-zinc-200 h-full text-black placeholder-black font-semibold text-lg rounded-xl my-auto mx-auto p-4 overflow-y-scroll"
-                                  placeholder="Write Or Generate Email Body"
-                                  value={data}
-                                  rows={4}
-                                  onChange={(e) => setData(e.target.value)}
-                                />
-                              </>
-                            )}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="w-full flex gap-4 right-0 mr-[50vh] md:gap-x-10 pt-2 p-2 mt-16 scale-75 md:scale-100 justify-center">
-                        <button
-                          className="px-4 py-2 md:py-3 rounded-lg border-2 border-neutral-100 text-black text-md font-bold bg-white hover:bg-transparent hover:text-white transition duration-200 text-xl focus-visible:font-white focus-visible:font-bold focus-visible:bg-transparent focus-visible:border-2-white focus-visible:text-white focus-visible:rounded-xl"
-                          onClick={callBackend}>
-                          Generate Email
-                        </button>
-                        <button
-                          className="px-8 rounded-lg border-2 border-neutral-100 text-black text-md font-bold bg-white hover:bg-transparent hover:text-white transition duration-200 text-xl focus-visible:font-white focus-visible:font-bold focus-visible:bg-transparent focus-visible:border-2-white focus-visible:text-white focus-visible:rounded-xl"
-                          onClick={sendEmail}>
-                          Send Email
-                        </button>
-                      </div>
+                <div className="flex flex-col justify-center md:scale-105">
+                  <div className="flex flex-col lg:flex-row md:col-auto gap-2 w-full ">
+                    <div className="flex flex-col flex-grow">
+                      <label htmlFor="">Name</label>
+                      <input
+                        type="name"
+                        placeholder="Enter Your Name"
+                        onChange={(e) => setSender(e.target.value)}
+                        value={user ? user.displayName : ""}
+                        className="placeholder-opacity-95 bg-zinc-200 placeholder-black text-lg rounded-xl text-black font-semibold p-4"
+                      />
+                    </div>
+                    <div className="flex flex-col flex-grow">
+                      <label htmlFor="">Sender&apos;s Email</label>
+                      <input
+                        type="email"
+                        placeholder="Enter Your Email Address"
+                        onChange={(e) => setSenderEmail(e.target.value)}
+                        value={user ? user.email : ""}
+                        className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
+                      />
+                    </div>
+                    <div className="flex flex-col flex-grow">
+                      <label htmlFor="">Recipient&apos;s Email</label>
+                      <input
+                        type="email"
+                        placeholder="Enter Recipient's Email Address"
+                        onChange={(e) => setReceiverEmail(e.target.value)}
+                        className="font-semibold text-left placeholder-opacity-95 rounded-xl bg-zinc-200 text-lg placeholder-black text-black p-4"
+                      />
                     </div>
                   </div>
+
+                  <br />
+                  <div>
+                    <label htmlFor="">Subject</label>
+                    <input
+                      className="w-full flex font-semibold justify-center text-lg items-center placeholder-opacity-95 rounded-xl bg-zinc-200 placeholder-black text-black p-4 text-md"
+                      placeholder="Enter the subject of the email"
+                      onChange={(e) => setSubject(e.target.value)}
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <div>
+                      <br />
+                      {loading && editable ? (
+                        <div>
+                          <label htmlFor="">Email Body</label>
+                          <textarea
+                            className="w-full bg-zinc-200 h-full text-black placeholder-black font-semibold text-lg rounded-xl my-auto mx-auto p-4 overflow-y-scroll"
+                            value={data}
+                            contentEditable="false"
+                            placeholder="Generating Email..."
+                            rows={6}
+                            onChange={(e) => setData(e.target.value)}
+                          />
+                        </div>
+                      ) : (
+                        <>
+                          <label htmlFor="">Email Body</label>
+                          <textarea
+                            className="w-full bg-zinc-200 h-full text-black placeholder-black font-semibold text-lg rounded-xl my-auto mx-auto p-4 overflow-y-scroll"
+                            placeholder="Write Or Generate Email Body"
+                            value={data}
+                            rows={4}
+                            onChange={(e) => setData(e.target.value)}
+                          />
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full flex gap-4 right-0 mr-[50vh] md:gap-x-10 pt-2 p-2 mt-16 scale-75 md:scale-100 justify-center">
+                  <button
+                    className="px-4 py-2 md:py-3 rounded-lg border-2 border-neutral-100 text-black text-md font-bold bg-white hover:bg-transparent hover:text-white transition duration-200 text-xl focus-visible:font-white focus-visible:font-bold focus-visible:bg-transparent focus-visible:border-2-white focus-visible:text-white focus-visible:rounded-xl"
+                    onClick={callBackend}>
+                    Generate Email
+                  </button>
+                  <button
+                    className="px-8 rounded-lg border-2 border-neutral-100 text-black text-md font-bold bg-white hover:bg-transparent hover:text-white transition duration-200 text-xl focus-visible:font-white focus-visible:font-bold focus-visible:bg-transparent focus-visible:border-2-white focus-visible:text-white focus-visible:rounded-xl"
+                    onClick={sendEmail}>
+                    Send Email
+                  </button>
+                </div>
+              </div>
+            </div>
           </>
         ) : (
           <div className="flex w-full h-full md:h-3/4 justify-center items-center mb-20">
-            <h1 className="text-3xl md:text-4xl text-white font-semibold">Please Login To Continue :)</h1>
+            <h1 className="text-3xl md:text-4xl text-white font-semibold">
+              Please Login To Continue :)
+            </h1>
           </div>
         )}
       </div>
